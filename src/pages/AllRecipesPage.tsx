b4e5f789800d177e64fd2b769b669ee987451844
Anastasiia <120492521+matoka02@users.noreply.github.com>
2024-11-30 15:13:15 +0200

    add pages

---
+++ src/pages/AllRecipesPage.tsx
@@ -0,0 +1,9 @@
+import React, { useEffect } from 'react';
+
+const AllRecipesPage: React.FC = () => {
+  return (
+    <h2>AllRecipesPage</h2>
+  )
+}
+
+export default AllRecipesPage;
---
+++ src/pages/SelectedRecipesPage.tsx
@@ -0,0 +1,9 @@
+import React, { useEffect } from 'react';
+
+const SelectedRecipesPage: React.FC = () => {
+  return (
+    <h2>SelectedRecipesPage</h2>
+  )
+}
+
+export default SelectedRecipesPage;
