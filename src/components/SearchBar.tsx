2040066c1621a25827ae98e40f126fc587fa2fc3
Anastasiia <120492521+matoka02@users.noreply.github.com>
2024-11-30 19:25:18 +0200

    update SearchBar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Form } from 'react-bootstrap';
+import { debounce } from '../utils/debounce';
 
 interface SearchBarProps {
   onSearch: (query: string) => void;
@@ -8,9 +9,14 @@ interface SearchBarProps {
 export const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
   const [searchTerm, setSearchTerm] = useState('');
 
+  const debouncedSearch = debounce((query: string) => {
+    onSearch(query);
+  }, 500); 
+
   const handleSearchChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchTerm(evt.target.value);
-    onSearch(evt.target.value);
+    const query = evt.target.value;
+    setSearchTerm(query);
+    debouncedSearch(query);
   };
 
   return (
