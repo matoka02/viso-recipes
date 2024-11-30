9404c53d85048adf274eadf1c98a9a46918cda13
Anastasiia <120492521+matoka02@users.noreply.github.com>
2024-11-30 19:31:06 +0200

    update Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,18 +3,18 @@ import { Navbar as BootstrapNavbar, Nav, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const Navbar: React.FC = () => (
-  <BootstrapNavbar bg='dark' variant='dark' expand='lg'>
+  <BootstrapNavbar bg="dark" variant="dark" expand="lg" className="shadow-sm">
     <Container>
-      {/* <BootstrapNavbar.Brand as={Link} to='/'>
+      <BootstrapNavbar.Brand as={Link} to="/" className="fw-bold">
         Recipe App
-      </BootstrapNavbar.Brand> */}
-      {/* <BootstrapNavbar.Toggle aria-controls='basic-navbar-nav' /> */}
-      <BootstrapNavbar.Collapse id='basic-navbar-nav'>
-        <Nav className='me-auto'>
-          <Nav.Link as={Link} to='/'>
+      </BootstrapNavbar.Brand>
+      <BootstrapNavbar.Toggle aria-controls="basic-navbar-nav" />
+      <BootstrapNavbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link as={Link} to="/" className="text-light  me-3">
             All Recipes
           </Nav.Link>
-          <Nav.Link as={Link} to='/selected'>
+          <Nav.Link as={Link} to="/selected" className="text-light  me-3">
             Selected Recipes
           </Nav.Link>
         </Nav>
